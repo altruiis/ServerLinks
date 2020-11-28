dc40f21f3015635a6022d37de5abd9194e0cf5fa
Altruiis <71913030+Altruiis@users.noreply.github.com>
2020-11-28 02:23:37 -0800

    Poob?

--- src/me/number3504/serverlinks/commands/CommandHandler.java
+++ src/me/number3504/serverlinks/commands/CommandHandler.java
@@ -9,14 +9,15 @@ import me.number3504.serverlinks.ServerLinksMain;
 import me.number3504.serverlinks.Utils;
 
 public class CommandHandler implements org.bukkit.command.CommandExecutor {
-
+	
 	private HashMap<String, CommandExecutor> commands = new HashMap<String, CommandExecutor>();
-
+	
 	public CommandHandler() {
 		commands.put("info", new ServerLinksCommand());
 	}
-
+	
 	static ServerLinksMain plugin;
+	
 
 	public CommandHandler(ServerLinksMain serverLinksMain) {
 		plugin = serverLinksMain;
@@ -24,16 +25,16 @@ public class CommandHandler implements org.bukkit.command.CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
+		
 		String name = cmd.getName().toLowerCase();
 		if (commands.containsKey(name)) {
 			final CommandExecutor command = commands.get(name);
-
+			
 			if (command.getPermission() != null && !sender.hasPermission(command.getPermission())) {
 				sender.sendMessage(Utils.msg(plugin.config.getString("noPermission")));
 			}
 		}
-
+		
 		return false;
 	}
 }
--- src/me/number3504/serverlinks/commands/ServerLinksCommand.java
+++ src/me/number3504/serverlinks/commands/ServerLinksCommand.java
@@ -6,27 +6,31 @@ import org.bukkit.command.CommandSender;
 import me.number3504.serverlinks.ServerLinksMain;
 
 public class ServerLinksCommand extends CommandExecutor {
-
+	
 	static ServerLinksMain main;
+	
 
 	public ServerLinksCommand(ServerLinksMain serverLinksMain) {
 		main = serverLinksMain;
 	}
-
+	
 	public ServerLinksCommand() {
 		setCommand("info");
 		setLength(1);
 	}
 
 	public void execute(CommandSender sender, String[] args) {
-		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Server running&3 ServerLinks"));
-		sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-				"&7Plugin version&3 " + main.getDescription().getVersion()));
-		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Plugin made by&3 Number3504"));
-		sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-				"&7If you like my plugin, consider leaving a &3review&7 on the SpigotMC page to help me out!"));
-		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7To find plugin commands, do &3/sl help"));
-
+			sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+					"&7Server running&3 ServerLinks"));
+			sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+					"&7Plugin version&3 " + main.getDescription().getVersion()));
+			sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+					"&7Plugin made by&3 Number3504"));
+			sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+					"&7If you like my plugin, consider leaving a &3review&7 on the SpigotMC page to help me out!"));
+			sender.sendMessage(
+					ChatColor.translateAlternateColorCodes('&', "&7To find plugin commands, do &3/sl help"));
+		
 	}
 
 }
