e00d15dc56e3c8bdbd86ea47e039a2dc4d0818c5
Altruiis <71913030+Altruiis@users.noreply.github.com>
2020-12-05 14:13:05 -0800

    Update info message
    - Replace spigot discussion tab with github issue page

--- src/me/number3504/serverlinks/commands/InfoCommand.java
+++ src/me/number3504/serverlinks/commands/InfoCommand.java
@@ -18,9 +18,9 @@ public class InfoCommand extends CommandExecutor {
 	public void execute(CommandSender sender, String[] args) {
 		sender.sendMessage(Utils.msg(main.getConfig().getString("messages.prefix")));
 		sender.sendMessage(Utils
-				.msg("&7Need assistance? Leave a reply on &3https://www.spigotmc.org/threads/serverlinks.271288/"));
+				.msg("&7Need assistance? Make an issue at &3https://github.com/Altruiis/ServerLinks/issues/"));
 		sender.sendMessage(Utils
-				.msg("&7Make sure to include relevant server info, like your &3Server version &7and &3console log"));
+				.msg("&7Make sure to include relevant server info, like your &3server version &7and &3error log"));
 		sender.sendMessage(
 				Utils.msg("&7You can also leave a review on &3https://www.spigotmc.org/resources/serverlinks.46807/"));
 		sender.sendMessage(Utils.msg("&7To find plugin commands, do &3/sl help"));
