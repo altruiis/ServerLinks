bc0d236a836afdd159d6517ec340d566433eace2
Altruiis <71913030+Altruiis@users.noreply.github.com>
2022-12-18 14:05:21 -0800

    Not needed

--- src/main/java/me/number3504/serverlinks/Utils.java
+++ src/main/java/me/number3504/serverlinks/Utils.java
@@ -1,11 +1,6 @@
 package me.number3504.serverlinks;
 
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-
 public class Utils {
-
-    private static final MiniMessage mm = MiniMessage.miniMessage();
 
     public static String cap(final String line) {
         return Character.toUpperCase(line.charAt(0)) + line.substring(1);
