e66c5b2a1b4b529e57176d3908a70800bc2849e4
Altruiis <71913030+Altruiis@users.noreply.github.com>
2022-12-18 13:34:11 -0800

    Start on minimessage rewrite

--- src/main/java/me/number3504/serverlinks/Utils.java
+++ src/main/java/me/number3504/serverlinks/Utils.java
@@ -1,26 +1,18 @@
 package me.number3504.serverlinks;
 
-import net.md_5.bungee.api.ChatColor;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class Utils {
 
-    private final static Pattern pattern = Pattern.compile("#[a-fA-f0-9]{6}");
-
-    public static String msg(String s) {
-        Matcher match = pattern.matcher(s);
-        while (match.find()) {
-            String color = s.substring(match.start(), match.end());
-            s = s.replace(color, ChatColor.of(color) + "");
-            match = pattern.matcher(s);
-        }
-        return ChatColor.translateAlternateColorCodes('&', s);
-    }
+    private static final MiniMessage mm = MiniMessage.miniMessage();
 
     public static String cap(final String line) {
         return Character.toUpperCase(line.charAt(0)) + line.substring(1);
+    }
+
+    public static void send(Audience audience, String message) {
+        audience.sendMessage(mm.deserialize(message));
     }
 
 }
