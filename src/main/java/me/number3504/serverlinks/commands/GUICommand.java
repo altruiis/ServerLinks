fb56ae22e318add41599748529976d7701867089
Altruiis <71913030+Altruiis@users.noreply.github.com>
2023-01-02 18:57:16 -0800

    Initial work

--- src/main/java/me/number3504/serverlinks/commands/GUICommand.java
+++ src/main/java/me/number3504/serverlinks/commands/GUICommand.java
@@ -5,12 +5,14 @@ import me.number3504.serverlinks.Utils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
 
@@ -18,34 +20,47 @@ public class GUICommand extends CommandExecutor {
 
     private final Main main;
     private final MiniMessage mm = MiniMessage.miniMessage();
+    private final NamespacedKey key;
 
     public GUICommand(Main main) {
         this.main = main;
         setCommand("gui");
         setLength(1);
+        this.key = new NamespacedKey(main, "SL-link");
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
         if (sender instanceof Player player) {
             ConfigurationSection section = main.getConfig().getConfigurationSection("links");
+            if (section == null) {
+                sender.sendRichMessage(main.getConfig().getString("messages.sectionNotFound"));
+                return;
+            }
             if (section.getKeys(false).stream().allMatch(s -> main.getConfig().getString("links." + s).isEmpty())) {
                 sender.sendRichMessage(main.getConfig().getString("messages.prefix") + main.getConfig().getString("messages.linksNotSet"));
                 return;
             }
-            Inventory inv = main.getServer().createInventory(null, 18, mm.deserialize(main.getConfig().getString("messages.guiName")));
-            int index = -1;
-            for (Object s : section.getKeys(false)) {
+            int size = main.getConfig().getInt("gui.size");
+            Component title = mm.deserialize(main.getConfig().getString("gui.title"));
+            Inventory inv = main.getServer().createInventory(null, size, title);
+            int index = 0;
+            for (String s : section.getKeys(false)) {
                 if (!main.getConfig().getString("links." + s).isEmpty()) {
-                    index++;
-                    ItemStack item = new ItemStack(Material.PAPER);
+                    ItemStack item = new ItemStack(Material.valueOf(main.getConfig().getString("gui.itemFormat.material")));
                     ItemMeta meta = item.getItemMeta();
-                    meta.displayName(mm.deserialize(main.getConfig().getString("messages.guiItemColor") + Utils.cap(s.toString())));
+                    String name = main.getConfig().getString("gui.itemFormat.name")
+                            .replace("%link%", s).replace("%link-capital%", Utils.cap(s));
+                    meta.displayName(mm.deserialize(name));
                     ArrayList<Component> lore = new ArrayList<>();
-                    lore.add(mm.deserialize(main.getConfig().getString("links." + s)));
+                    String link = main.getConfig().getString("links." + s);
+                    for (String toAdd : main.getConfig().getStringList("gui.itemFormat.lore"))
+                        lore.add(mm.deserialize(toAdd.replace("%value%", link).replace("%value-unformatted%", mm.stripTags(link))));
                     meta.lore(lore);
+                    meta.getPersistentDataContainer().set(key, PersistentDataType.STRING, s);
                     item.setItemMeta(meta);
                     inv.setItem(index, item);
+                    index++;
                 }
             }
             player.openInventory(inv);
--- src/main/java/me/number3504/serverlinks/listeners/InventoryClickListener.java
+++ src/main/java/me/number3504/serverlinks/listeners/InventoryClickListener.java
@@ -1,25 +1,46 @@
 package me.number3504.serverlinks.listeners;
 
 import me.number3504.serverlinks.Main;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
 
 public class InventoryClickListener implements Listener {
 
     private final Main main;
+    private final MiniMessage mm = MiniMessage.miniMessage();
+    private final NamespacedKey key;
 
     public InventoryClickListener(Main main) {
         this.main = main;
+        this.key = new NamespacedKey(main, "SL-link");
     }
 
     @EventHandler
     public void onInvClick(InventoryClickEvent e) {
         Player player = (Player) e.getWhoClicked();
-        if (e.getView().getTitle().equalsIgnoreCase(main.getConfig().getString("messages.guiName"))) {
+        if (e.getView().title().equals(mm.deserialize(main.getConfig().getString("gui.title")))) {
             e.setCancelled(true);
-            player.sendMessage(e.getCurrentItem().getItemMeta().lore().get(0));
+            if (e.getCurrentItem() == null) return;
+            ItemStack item = e.getCurrentItem();
+            if (!main.getConfig().getString("gui.clickMessage").isEmpty()) {
+                String link = item.getItemMeta().getPersistentDataContainer().get(key, PersistentDataType.STRING);
+                String value = main.getConfig().getString("links." + link);
+                String msg = main.getConfig().getString("gui.clickMessage")
+                        .replace("%link%", value)
+                        .replace("%link-unformatted%", mm.stripTags(value));
+                if (!msg.isEmpty()) {
+                    player.sendRichMessage(msg);
+                }
+                if (main.getConfig().getBoolean("gui.closeOnClick")) {
+                    main.getServer().getScheduler().runTask(main, () -> player.closeInventory());
+                }
+            }
         }
     }
 
