db9e68af4fcf1545df19ba434f3081f3b3925224
Altruiis <71913030+Altruiis@users.noreply.github.com>
2023-01-06 16:49:36 -0800

    Add link to modrinth

--- src/main/java/me/number3504/serverlinks/commands/InfoCommand.java
+++ src/main/java/me/number3504/serverlinks/commands/InfoCommand.java
@@ -13,13 +13,12 @@ public class InfoCommand extends CommandExecutor {
         setLength(1);
     }
 
-    // TODO: Modrinth link
     public void execute(CommandSender sender, String[] args) {
         sender.sendRichMessage(main.getConfig().getString("messages.prefix"));
         sender.sendRichMessage("<gray>Need assistance? Make an issue on<dark_aqua> " +
-                "<hover:show_text:'<aqua>Click to go there now'><click:open_url:'https://github.com/Altruiis/ServerLinks/'><u>GitHub");
+                "<hover:show_text:'<aqua>Click to visit!'><click:open_url:'https://github.com/Altruiis/ServerLinks/'><u>GitHub");
         sender.sendRichMessage("<gray>Make sure to include relevant server info, like your <dark_aqua>server version <gray>and <dark_aqua>error log");
-        sender.sendRichMessage("<gray>You can view the plugin on <dark_aqua>");
+        sender.sendRichMessage("<gray>You can view the plugin on <hover:show_text:'<aqua>Click to visit!'><click:open_url:'https://modrinth.com/plugin/serverlinks'><dark_aqua><u>Modrinth");
         sender.sendRichMessage("<gray>To find plugin commands, do <hover:show_text:'<aqua><u>Click!'><click:run_command:'/sl help'><dark_aqua><u>/sl help");
     }
 
